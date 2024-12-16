e607afed5569bbe85321ef577ca36ff0cfc68a82
Emad <eksaber87@gmail.com>
2024-12-16 13:49:08 +0100

    clase

---
+++ quiz.js
@@ -0,0 +1,12 @@
+document.querySelector(opcions).children 
+
+const handleClickAnswer = (event) => {
+    const elementButton = 
+
+}
+
+
+optionButtons.forEach((button) => button.addEventListener('click', handleClickAnswer)) //La funcion se lee al reves, el nombre es handleClckAnawer y hace lo de atras, tener un addlistener que escucha botton con un buclefor Each, esto es una callback, se llama despues como dice el nombre!
+
+
+
